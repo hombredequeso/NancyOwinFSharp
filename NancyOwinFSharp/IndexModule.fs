75f3677a5f8a7a5ff12118c1d25a8a34b4687a29
Mark Cheeseman <mcheesemanau@gmail.com>
2015-05-20 09:48:00 +1000

    Database Test in Azure
    adding /dbtest route, trying out SqlDataConnection in Azure (probably
    not going to work)

--- NancyOwinFSharp/IndexModule.fs
+++ NancyOwinFSharp/IndexModule.fs
@@ -2,7 +2,25 @@
 
 open Nancy
 
+open Microsoft.FSharp.Data.TypeProviders
+
+type MercuryDb = SqlDataConnection<ConnectionStringName = @"MercuryDb">
+
 type IndexModule() as x =
     inherit NancyModule()
     do x.Get.["/"] <- fun _ -> box x.View.["index"]
+
+    do x.Get.["/dbtest"] <- fun _ ->
+        
+        use dbContext = MercuryDb.GetDataContext()
+        let agentBookingStatusTable = dbContext.AgentBookingStatus
+
+        let statusQuery = query {
+            for row in agentBookingStatusTable do
+            select row
+        }
+
+        let statuses = statusQuery |> Seq.toList
+        let response = x.Response.AsJson(statuses)
+        response :> obj
 
